7e95ef1668cd8866c8f97fa8dfd524b987888afb
s1270146 <s1270146@gmail.com>
2024-10-05 14:14:30 +0900

    feat: #8 create profile page

---
+++ lib/src/layout/sidebar.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mk_portfolio/resources/colors.dart';
+import 'package:mk_portfolio/src/controllers/provider.dart';
+import 'package:mk_portfolio/src/widget/command_text.dart';
+
+class Sidebar extends ConsumerWidget {
+  const Sidebar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pageController = ref.watch(pageControllerProvider);
+    return Container(
+      color: AppColors.backgroundColor,
+      child: Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CommandText(
+              text: "tree",
+              fontSize: 24,
+            ),
+            Text(
+              "/",
+              style: TextStyle(
+                color: AppColors.primaryColor,
+                fontSize: 24,
+              ),
+            ),
+            Row(
+              children: [
+                Text(
+                  "├──",
+                  style: TextStyle(
+                    color: AppColors.primaryColor,
+                    fontSize: 24,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    pageController.animateToPage(
+                      0,
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                  child: Text(
+                    "Profile",
+                    style: TextStyle(
+                      color: AppColors.primaryColor,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  "├──",
+                  style: TextStyle(
+                    color: AppColors.primaryColor,
+                    fontSize: 24,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    pageController.animateToPage(
+                      1,
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                  child: Text(
+                    "Skills",
+                    style: TextStyle(
+                      color: AppColors.primaryColor,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  "├──",
+                  style: TextStyle(
+                    color: AppColors.primaryColor,
+                    fontSize: 24,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    pageController.animateToPage(
+                      2,
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                  child: Text(
+                    "Works",
+                    style: TextStyle(
+                      color: AppColors.primaryColor,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  "└──",
+                  style: TextStyle(
+                    color: AppColors.primaryColor,
+                    fontSize: 24,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    pageController.animateToPage(
+                      3,
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                  child: Text(
+                    "Blog",
+                    style: TextStyle(
+                      color: AppColors.primaryColor,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
