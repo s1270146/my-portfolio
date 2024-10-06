68605ab83db7d2bf15f1575c62ddde7ff0bfec27
s1270146 <s1270146@gmail.com>
2024-10-06 10:16:54 +0900

    feat: #16 accounts

--- lib/src/layout/profile_view.dart
+++ lib/src/layout/profile_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mk_portfolio/resources/colors.dart';
+import 'package:mk_portfolio/src/widget/account_image.dart';
+import 'package:mk_portfolio/src/widget/circle_image.dart';
 import 'package:mk_portfolio/src/widget/command_text.dart';
+import 'package:mk_portfolio/src/widget/command_title.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -12,121 +15,147 @@ class ProfileView extends StatelessWidget {
         top: 20,
         left: 20,
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CommandText(text: "pwd"),
-          Text(
-            "/Profile",
-            style: TextStyle(
-              fontSize: 36,
-              color: AppColors.primaryColor,
-            ),
-          ),
-          CommandText(text: "profileFetch"),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  flex: 5,
-                  child: Center(
-                    child: ClipOval(
-                      child: Image.asset(
-                        'images/profile.jpg',
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CommandTitle(text: "Profile"),
+            CommandText(text: "profileFetch"),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: Center(
+                      child: CircleImage(
+                        imagePath: "assets/images/profile.jpg",
                         width: 300.0,
                         height: 300.0,
-                        fit: BoxFit.cover,
                       ),
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 5,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Wrap(
-                        children: [
-                          Text(
-                            "Name: ",
-                            style: TextStyle(
-                              fontSize: 28,
-                              color: AppColors.secondColor,
+                  Expanded(
+                    flex: 5,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Wrap(
+                          children: [
+                            Text(
+                              "Name: ",
+                              style: TextStyle(
+                                fontSize: 28,
+                                color: AppColors.secondColor,
+                              ),
+                            ),
+                            Text(
+                              "Koyo Mori",
+                              style: TextStyle(
+                                fontSize: 28,
+                                color: AppColors.primaryColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Wrap(
+                          children: [
+                            Text(
+                              "Grade: ",
+                              style: TextStyle(
+                                fontSize: 28,
+                                color: AppColors.secondColor,
+                              ),
+                            ),
+                            Text(
+                              "Master's Student",
+                              style: TextStyle(
+                                fontSize: 28,
+                                color: AppColors.primaryColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Wrap(
+                          children: [
+                            Text(
+                              "Job: ",
+                              style: TextStyle(
+                                fontSize: 28,
+                                color: AppColors.secondColor,
+                              ),
+                            ),
+                            Text(
+                              "Software Engineer (Part-time)",
+                              style: TextStyle(
+                                fontSize: 28,
+                                color: AppColors.primaryColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Wrap(
+                          children: [
+                            Text(
+                              "Hobby: ",
+                              style: TextStyle(
+                                fontSize: 28,
+                                color: AppColors.secondColor,
+                              ),
+                            ),
+                            Text(
+                              "Soccer, HTB, CTF",
+                              style: TextStyle(
+                                fontSize: 28,
+                                color: AppColors.primaryColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          "Accounts:",
+                          style: TextStyle(
+                            fontSize: 28,
+                            color: AppColors.secondColor,
+                          ),
+                        ),
+                        Container(
+                          color: Colors.white,
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Wrap(
+                              spacing: 10.0,
+                              children: [
+                                AccountImage(
+                                  imagePath: "assets/images/x.png",
+                                  url: "https://x.com/mk__record",
+                                ),
+                                AccountImage(
+                                  imagePath: "assets/images/github.png",
+                                  url: "https://github.com/s1270146",
+                                ),
+                                AccountImage(
+                                  imagePath: "assets/images/zenn.png",
+                                  url: "https://zenn.dev/pascal",
+                                ),
+                                AccountImage(
+                                  imagePath: "assets/images/mk-record.png",
+                                  url: "https://mk-record.com",
+                                ),
+                              ],
                             ),
                           ),
-                          Text(
-                            "Koyo Mori",
-                            style: TextStyle(
-                              fontSize: 28,
-                              color: AppColors.primaryColor,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Wrap(
-                        children: [
-                          Text(
-                            "Grade: ",
-                            style: TextStyle(
-                              fontSize: 28,
-                              color: AppColors.secondColor,
-                            ),
-                          ),
-                          Text(
-                            "Master's Student",
-                            style: TextStyle(
-                              fontSize: 28,
-                              color: AppColors.primaryColor,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Wrap(
-                        children: [
-                          Text(
-                            "Job: ",
-                            style: TextStyle(
-                              fontSize: 28,
-                              color: AppColors.secondColor,
-                            ),
-                          ),
-                          Text(
-                            "Software Engineer (Part-time)",
-                            style: TextStyle(
-                              fontSize: 28,
-                              color: AppColors.primaryColor,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Wrap(
-                        children: [
-                          Text(
-                            "Hobby: ",
-                            style: TextStyle(
-                              fontSize: 28,
-                              color: AppColors.secondColor,
-                            ),
-                          ),
-                          Text(
-                            "Soccer, HTB, CTF",
-                            style: TextStyle(
-                              fontSize: 28,
-                              color: AppColors.primaryColor,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
